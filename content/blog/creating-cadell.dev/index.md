4f729adf78586049e613aa9f8ce3351395e69b13
Cadell Christo <cadellchristo>
2019-04-10 20:59:59 +0800

    Finished post formatting

--- content/blog/creating-cadell.dev/index.md
+++ content/blog/creating-cadell.dev/index.md
@@ -2,29 +2,43 @@
 
 6:20pm 10/4/2019
 
-1. Open https://typora.io/ so you can write your first blog post as you go.
+Duration:  2h 40m while writing this post.
 
-2. Go to https://domains.google.com to buy the .dev domain. Its a bit cheaper than namecheap.
+## Introduction
+
+There was some [rave reviews](https://news.ycombinator.com/item?id=19615546) for Netlify on hackernews this morning. It really did sound awesome - static hosting with a CDN, branch deployments, [Netlify CMS](https://www.netlifycms.org/) and [Netlify Functions](https://www.netlify.com/docs/functions/) all for free.
+
+Combined with my mate, Franc, telling me to snatch up cadell.dev led me to create this website and post.
+
+The Netlify experience was fantastic! A few decisions along the way but overall very seamless.
+
+The biggest decision to make was how to use Gatsby - should I use the blog starter pack or the basic starter pack. This was a bit of a time sink. I opted for the base starter pack and manual markdown file conversions for now.
+
+## Method
+
+1. Open [Typora]([https://typora.io/) so you can write your first blog post as you go.
+
+2. Go to [domains.google.com](https://domains.google.com) to buy the .dev domain. Its a bit cheaper than namecheap.
 
    1. Verify the domain by clicking the link in your emails.
    2. Leave this tab open for later.
 
-3. Go to https://github.com to create the repo.
+3. Go to [GitHub](https://github.com) to create the repo.
 
    1. Click the plus on the top right corner, then go New repository.
    2. Name it cadell.dev.
    3. Click Create Repository.
    4. Leave the tab open.
 
-4. Go to https://www.gatsbyjs.org/
+4. Go to [GatsbyJS](https://www.gatsbyjs.org/).
 
-   1. https://www.gatsbyjs.org/docs/quick-start
+   1. Open the [Quick start](https://www.gatsbyjs.org/docs/quick-start).
 
    2. Open Terminal.
 
-   3. cd to your personal dev folder. ```cd dev/personal```
+   3. cd to your personal dev folder ```cd dev/personal```.
 
-   4. Install the gatsby CLI
+   4. Install the gatsby CLI.
 
    5. ```shell
       npm install -g gatsby-cli
@@ -34,19 +48,19 @@
 
       ```gatsby new cadell.dev ```
 
-   7. cd into cadell.dev ```cd cadell.dev```
+   7. cd into cadell.dev ```cd cadell.dev```.
 
-   8. start gatsby ```gatsby develop```
+   8. Start gatsby ```gatsby develop.```
 
-   9. Open localhost:8000 in your browser
+   9. Open localhost:8000 in your browser.
 
-   10. close gatsby by clicking control+c
+   10. Close gatsby by typing ```control + c```.
 
-5. Open the github page you left open earlier
+5. Open the github page you left open earlier.
 
    1. Copy the command from the "or push an exsting repository from the command line" section. Gatsby created a git repo for us.
 
-   2. run the command in terminal
+   2. Run the command in terminal.
 
       ```shell
       git remote add origin https://github.com/cadbox1/cadell.dev.git
@@ -55,9 +69,9 @@
 
    3. Refresh the github page to see the files from our first commit.
 
-6. Go to https://www.netlify.com/
+6. Go to [Netlify](https://www.netlify.com/).
 
-   1. Signup
+   1. Signup.
    2. Activate your account via email to get in (I like this).
    3. On the Create a new site page, Click connect to Git provider.
    4. Click GitHub.
@@ -68,14 +82,14 @@
    9. Netlify has automatically detected that our site is a Gatsby site. Click Deploy site.
    10. Wait a few moments for the site to be deployed.
    11. Click Set up a custom domain. Apparently you can buy the domain inside Netlify, for ~$5 aud extra. Oh well, setting up DNS is somewhat related to being a web developer.
-   12. Type in cadell.dev
+   12. Type in ```cadell.dev```.
    13. Click Yes, add domain.
    14. Click Check DNS configuration.
-   15. Now apparently we can either do the developer thing I spoke about earlier and setup the DNS in Google or get Netlify to manage our DNS. The latter will mean that we can have branch deploys under our domain which sounds aesthetically better. https://www.netlify.com/docs/continuous-deployment/#branches-deploys. We still get to configure name servers which is still pretty developery
-   16. Click Set up Netlify DNS for cadell.dev
-   17. Click Verify
+   15. Now apparently we can either do the developer thing I spoke about earlier and setup the DNS in Google or get Netlify to manage our DNS. The latter will mean that we have the option to have [branch deploys under our domain](https://www.netlify.com/docs/continuous-deployment/#branches-deploys) which sounds aesthetically better. We still get to configure name servers which is still pretty developery.
+   16. Click Set up Netlify DNS for cadell.dev.
+   17. Click Verify.
    18. Click Yes, add domain.
-   19. Since Netlify will now be our DNS, we have the option to add DNS records if we use the domain for other services like email. Let's skip this since we can always do it later.
+   19. Since Netlify will now be our DNS, we have the option to add DNS records if we use the domain for other services like email. Let's skip this since we aren't and can always do it later.
    20. Click Continue.
    21. Open up the Google Domains tab we left open from earlier.
    22. Click Use custom name servers for your domain.
@@ -92,14 +106,14 @@
    33. Select All for Branch deploys.
    34. Click Save.
    35. Click on Domain management to see that our SSL certificate has been created.
-   36. We can now go to https://cadell.dev/
-   37. Apparently you can also choose a starter to setup a git repo for us https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-default
+   36. We can now go to [https://cadell.dev/](https://cadell.dev/).
+   37. Apparently you can also [choose a starter to start from](https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-default).
 
 7. Open VSCode.
 
    1. Open the cadell.dev folder.
    2. Hit Control+` to reveal the terminal.
-   3. Create a new branch ```git checkout -b FirstRealCommit```
+   3. Create a new branch ```git checkout -b FirstRealCommit```.
    4. Dilemna: do I use markdown and if I do should I use the other starter or add it manually? Stuff it let's just convert our markdown to html in typora so I can add cool stuff in code later. Still would be good to save the markdown though.
    5. Create the following folders, starting at the root. ```content/blog/creating-cadell.dev```.
    6. Open Typora.
@@ -124,5 +138,16 @@
    25. Command+Click the layout component to see how it works.
    26. Do the same for the Header component.
    27. Lets change the header colour by changing the style.background property of the <header to ```hsl(0, 90%, 70.4%)```.
-   28. 
+   28. Click the Source Control tab on the left in VSCode and commit your work with the message ```added the first page```.
+   29. Click the cloud in the bottom left of VSCode to push the branch.
+   30. We should be able to go to [https://firstrealcommit--heuristic-jepsen-9bfaa6.netlify.com/](https://firstrealcommit--heuristic-jepsen-9bfaa6.netlify.com/) to see the result. Netlify built and deployed the branch automatically!
+   31. Click Output then Git to see the push result.
+   32. Command Click the pull request link to create a pull request.
+   33. Click Create pull request. This will kick off Netlify again.
+   34.  Wait a few minutes for Netlify to build. 
+   35. Click the deploy preview to see the site.
+   36. Click the drop down on the merge pull request then click Squash and merge.
+   37. Go to [https://cadell.dev/](https://cadell.dev/) and see the result.
+
+Finish time: 9:00pm (2h 40m)
 
--- src/articles/creating-cadell.dev.js
+++ src/articles/creating-cadell.dev.js
@@ -5,23 +5,43 @@ const CreatingCadellDev = () => {
     <div>
       <h1>Creating cadell.dev</h1>
       <p>6:20pm 10/4/2019</p>
+      <p>Duration: 2h 40m while writing this post.</p>
+      <h2>Introduction</h2>
+      <p>
+        There was some{" "}
+        <a href="https://news.ycombinator.com/item?id=19615546">rave reviews</a>{" "}
+        for Netlify on hackernews this morning. It really did sound awesome -
+        static hosting with a CDN, branch deployments,{" "}
+        <a href="https://www.netlifycms.org/">Netlify CMS</a> and{" "}
+        <a href="https://www.netlify.com/docs/functions/">Netlify Functions</a>{" "}
+        all for free.
+      </p>
+      <p>
+        Combined with my mate, Franc, telling me to snatch up cadell.dev led me
+        to create this website and post.
+      </p>
+      <p>
+        The Netlify experience was fantastic! A few decisions along the way but
+        overall very seamless.
+      </p>
+      <p>
+        The biggest decision to make was how to use Gatsby - should I use the
+        blog starter pack or the basic starter pack. This was a bit of a time
+        sink. I opted for the base starter pack and manual markdown file
+        conversions for now.
+      </p>
+      <h2>Method</h2>
       <ol>
         <li>
           <p>
-            Open{" "}
-            <a href="https://typora.io/" target="_blank" class="url">
-              https://typora.io/
-            </a>{" "}
-            so you can write your first blog post as you go.
+            Open <a href="%5Bhttps://typora.io/">Typora</a> so you can write
+            your first blog post as you go.
           </p>
         </li>
         <li>
           <p>
-            Go to{" "}
-            <a href="https://domains.google.com" target="_blank" class="url">
-              https://domains.google.com
-            </a>{" "}
-            to buy the .dev domain. Its a bit cheaper than namecheap.
+            Go to <a href="https://domains.google.com">domains.google.com</a> to
+            buy the .dev domain. Its a bit cheaper than namecheap.
           </p>
           <ol>
             <li>Verify the domain by clicking the link in your emails.</li>
@@ -30,11 +50,7 @@ const CreatingCadellDev = () => {
         </li>
         <li>
           <p>
-            Go to{" "}
-            <a href="https://github.com" target="_blank" class="url">
-              https://github.com
-            </a>{" "}
-            to create the repo.
+            Go to <a href="https://github.com">GitHub</a> to create the repo.
           </p>
           <ol>
             <li>
@@ -47,21 +63,16 @@ const CreatingCadellDev = () => {
         </li>
         <li>
           <p>
-            Go to{" "}
-            <a href="https://www.gatsbyjs.org/" target="_blank" class="url">
-              https://www.gatsbyjs.org/
-            </a>
+            Go to <a href="https://www.gatsbyjs.org/">GatsbyJS</a>.
           </p>
           <ol>
             <li>
               <p>
-                <a
-                  href="https://www.gatsbyjs.org/docs/quick-start"
-                  target="_blank"
-                  class="url"
-                >
-                  https://www.gatsbyjs.org/docs/quick-start
+                Open the{" "}
+                <a href="https://www.gatsbyjs.org/docs/quick-start">
+                  Quick start
                 </a>
+                .
               </p>
             </li>
             <li>
@@ -69,11 +80,11 @@ const CreatingCadellDev = () => {
             </li>
             <li>
               <p>
-                cd to your personal dev folder. <code>cd dev/personal</code>
+                cd to your personal dev folder <code>cd dev/personal</code>.
               </p>
             </li>
             <li>
-              <p>Install the gatsby CLI</p>
+              <p>Install the gatsby CLI.</p>
             </li>
             <li>
               <pre>
@@ -90,24 +101,26 @@ const CreatingCadellDev = () => {
             </li>
             <li>
               <p>
-                cd into cadell.dev <code>cd cadell.dev</code>
+                cd into cadell.dev <code>cd cadell.dev</code>.
               </p>
             </li>
             <li>
               <p>
-                start gatsby <code>gatsby develop</code>
+                Start gatsby <code>gatsby develop.</code>
               </p>
             </li>
             <li>
-              <p>Open localhost:8000 in your browser</p>
+              <p>Open localhost:8000 in your browser.</p>
             </li>
             <li>
-              <p>close gatsby by clicking control+c</p>
+              <p>
+                Close gatsby by typing <code>control + c</code>.
+              </p>
             </li>
           </ol>
         </li>
         <li>
-          <p>Open the github page you left open earlier</p>
+          <p>Open the github page you left open earlier.</p>
           <ol>
             <li>
               <p>
@@ -117,7 +130,7 @@ const CreatingCadellDev = () => {
               </p>
             </li>
             <li>
-              <p>run the command in terminal</p>
+              <p>Run the command in terminal.</p>
               <pre>
                 <code class="language-shell" lang="shell">
                   git remote add origin
@@ -135,13 +148,10 @@ const CreatingCadellDev = () => {
         </li>
         <li>
           <p>
-            Go to{" "}
-            <a href="https://www.netlify.com/" target="_blank" class="url">
-              https://www.netlify.com/
-            </a>
+            Go to <a href="https://www.netlify.com/">Netlify</a>.
           </p>
           <ol>
-            <li>Signup</li>
+            <li>Signup.</li>
             <li>Activate your account via email to get in (I like this).</li>
             <li>
               On the Create a new site page, Click connect to Git provider.
@@ -161,31 +171,29 @@ const CreatingCadellDev = () => {
               inside Netlify, for ~$5 aud extra. Oh well, setting up DNS is
               somewhat related to being a web developer.
             </li>
-            <li>Type in cadell.dev</li>
+            <li>
+              Type in <code>cadell.dev</code>.
+            </li>
             <li>Click Yes, add domain.</li>
             <li>Click Check DNS configuration.</li>
             <li>
               Now apparently we can either do the developer thing I spoke about
               earlier and setup the DNS in Google or get Netlify to manage our
-              DNS. The latter will mean that we can have branch deploys under
-              our domain which sounds aesthetically better.{" "}
-              <a
-                href="https://www.netlify.com/docs/continuous-deployment/#branches-deploys"
-                target="_blank"
-                class="url"
-              >
-                https://www.netlify.com/docs/continuous-deployment/#branches-deploys
-              </a>
-              . We still get to configure name servers which is still pretty
-              developery
+              DNS. The latter will mean that we have the option to have{" "}
+              <a href="https://www.netlify.com/docs/continuous-deployment/#branches-deploys">
+                branch deploys under our domain
+              </a>{" "}
+              which sounds aesthetically better. We still get to configure name
+              servers which is still pretty developery.
             </li>
-            <li>Click Set up Netlify DNS for cadell.dev</li>
-            <li>Click Verify</li>
+            <li>Click Set up Netlify DNS for cadell.dev.</li>
+            <li>Click Verify.</li>
             <li>Click Yes, add domain.</li>
             <li>
               Since Netlify will now be our DNS, we have the option to add DNS
               records if we use the domain for other services like email.
-              Let&#39;s skip this since we can always do it later.
+              Let&#39;s skip this since we aren&#39;t and can always do it
+              later.
             </li>
             <li>Click Continue.</li>
             <li>Open up the Google Domains tab we left open from earlier.</li>
@@ -216,20 +224,14 @@ const CreatingCadellDev = () => {
             </li>
             <li>
               We can now go to{" "}
-              <a href="https://cadell.dev/" target="_blank" class="url">
-                https://cadell.dev/
-              </a>
+              <a href="https://cadell.dev/">https://cadell.dev/</a>.
             </li>
             <li>
-              Apparently you can also choose a starter to setup a git repo for
-              us{" "}
-              <a
-                href="https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-default"
-                target="_blank"
-                class="url"
-              >
-                https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-default
+              Apparently you can also{" "}
+              <a href="https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-default">
+                choose a starter to start from
               </a>
+              .
             </li>
           </ol>
         </li>
@@ -239,11 +241,29 @@ const CreatingCadellDev = () => {
             <li>Open the cadell.dev folder.</li>
             <li>Hit Control+` to reveal the terminal.</li>
             <li>
-              Create a new branch <code>git checkout -b FirstRealCommit</code>
+              Create a new branch <code>git checkout -b FirstRealCommit</code>.
             </li>
             <li>
-              Run <code>yarn start</code> to start the development server. This
-              is standard for npm projects.
+              Dilemna: do I use markdown and if I do should I use the other
+              starter or add it manually? Stuff it let&#39;s just convert our
+              markdown to html in typora so I can add cool stuff in code later.
+              Still would be good to save the markdown though.
+            </li>
+            <li>
+              Create the following folders, starting at the root.{" "}
+              <code>content/blog/creating-cadell.dev</code>.
+            </li>
+            <li>Open Typora.</li>
+            <li>
+              Save the markdown file to{" "}
+              <code>content/blog/creating-cadell.dev/index.md</code>.
+            </li>
+            <li>Click File, Export, HTML without styles.</li>
+            <li>Open the file in VSCode.</li>
+            <li>Copy the code inside the body.</li>
+            <li>
+              Run <code>yarn start</code> in the terminal to start the
+              development server. This is standard for npm projects.
             </li>
             <li>
               Cmd+Click the{" "}
@@ -252,14 +272,74 @@ const CreatingCadellDev = () => {
               </a>{" "}
               link in terminal to open it in your browser.
             </li>
+            <li>Open gatsby-config.js</li>
+            <li>Change the siteMetadata.title to cadell.dev.</li>
             <li>
               Double click the index.js file under src/pages. The double click
               makes the tab &#39;sticky&#39;.
             </li>
-            <li />
+            <li>
+              Create a new file at{" "}
+              <code>src/articles/creating-cadell.dev.js</code>.
+            </li>
+            <li>
+              <code>rss</code> triggers the react snippets plugin in vscode,
+              name it <code>CreatingCadellDev</code>.
+            </li>
+            <li>Paste your html inside the div.</li>
+            <li>Right click, Format document to format the document.</li>
+            <li>Go back to the index.js file we left open.</li>
+            <li>
+              Remove everything inside the Layout except for the SEO component.
+            </li>
+            <li>
+              Start typing <code>&lt;CreatingCadellDev /&gt;</code> and click
+              the autocomplete to import it.
+            </li>
+            <li>Click Save and watch the page update.</li>
+            <li>Update the SEO component props as required.</li>
+            <li>Command+Click the layout component to see how it works.</li>
+            <li>Do the same for the Header component.</li>
+            <li>
+              Lets change the header colour by changing the style.background
+              property of the &lt;header to <code>hsl(0, 90%, 70.4%)</code>.
+            </li>
+            <li>
+              Click the Source Control tab on the left in VSCode and commit your
+              work with the message <code>added the first page</code>.
+            </li>
+            <li>
+              Click the cloud in the bottom left of VSCode to push the branch.
+            </li>
+            <li>
+              We should be able to go to{" "}
+              <a href="https://firstrealcommit--heuristic-jepsen-9bfaa6.netlify.com/">
+                https://firstrealcommit--heuristic-jepsen-9bfaa6.netlify.com/
+              </a>{" "}
+              to see the result. Netlify built and deployed the branch
+              automatically!
+            </li>
+            <li>Click Output then Git to see the push result.</li>
+            <li>
+              Command Click the pull request link to create a pull request.
+            </li>
+            <li>
+              Click Create pull request. This will kick off Netlify again.
+            </li>
+            <li> Wait a few minutes for Netlify to build. </li>
+            <li>Click the deploy preview to see the site.</li>
+            <li>
+              Click the drop down on the merge pull request then click Squash
+              and merge.
+            </li>
+            <li>
+              Go to <a href="https://cadell.dev/">https://cadell.dev/</a> and
+              see the result.
+            </li>
           </ol>
         </li>
       </ol>
+      <p>Finish time: 9:00pm (2h 40m)</p>
     </div>
   )
 }
