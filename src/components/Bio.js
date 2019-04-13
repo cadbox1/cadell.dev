b7149172030eee08bae4c5f33c73529ca50311e5
cadbox1 <cadellchristo@gmail.com>
2019-04-13 23:23:45 +0800

    used Gatsby Starter Blog MDX (#3)
    * used Gatsby Starter Blog MDX

---
+++ src/components/Bio.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { rhythm } from '../utils/typography'
+
+const Bio = () => (
+  <div>
+    <p>
+      Written by <strong>Cadell Christo</strong>.
+    </p>
+  </div>
+)
+
+export default Bio
