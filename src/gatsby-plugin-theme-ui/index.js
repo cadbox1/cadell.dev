eb105dbd641293e21b2d1e0a2fbba1af6dd54e81
cadbox1 <cadellchristo@gmail.com>
2020-02-23 20:09:04 +0800

    New home page (#7)
    * first commit of gatsby-starter-blog-theme
    
    * current status
    
    * refined the style scales on the home page
    
    * got most of the styles looking good now
    
    * adjust the fonts
    
    * adjust the colours

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,44 @@
+import baseTheme from "gatsby-theme-blog/src/gatsby-plugin-theme-ui";
+
+import "typeface-source-sans-pro";
+
+export default {
+	...baseTheme,
+	...{
+		fonts: {
+			body:
+				'"source sans pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial',
+			heading:
+				'"source sans pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial',
+			monospace: `Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace`,
+		},
+		space: [0, 4, 8, 12, 16, 24, 32, 64, 96, 128],
+		sizes: {
+			container: 850,
+		},
+		fontSizes: [16, 18, 21, 24, 32, 36, 44],
+		lineHeights: {
+			body: 1.7,
+			heading: 1.2,
+		},
+		fontWeights: {
+			body: 400,
+			heading: 600,
+			bold: 600,
+		},
+		styles: {
+			...baseTheme.styles,
+			a: {
+				...baseTheme.styles.a,
+				textDecoration: "none",
+				":active, :hover": {
+					textDecoration: "underline",
+				},
+			},
+			pre: {
+				...baseTheme.styles.pre,
+				fontSize: 0,
+			},
+		},
+	},
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,99 +1,46 @@
-import React, { Fragment } from 'react'
-import { Link, graphql } from 'gatsby'
+/** @jsx jsx */
+import { jsx, Styled } from "theme-ui";
+import { graphql } from "gatsby";
+import Layout from "gatsby-theme-blog/src/components/layout";
 
-import SEO from '../components/seo'
-import { headerBackgroundColor, centered } from '../utils/styles'
-import StickyHeader from '../components/StickyHeader'
+export default ({ location, data }) => (
+	<Layout location={location} title={data.site.siteMetadata.title}>
+		<div
+			sx={{
+				maxWidth: "container",
+				mx: "auto",
+				mt: 7,
+			}}
+		>
+			<Styled.h1 sx={{ fontSize: 6, mb: 4 }}>
+				I build awesome teams that build awesome products.
+			</Styled.h1>
+			<Styled.p sx={{ fontSize: 3 }}>
+				I'm a developer that takes customer problems from product requirements
+				and technical design, through development and deployment, all the way to
+				validation and customer support.
+			</Styled.p>
+		</div>
+		<Styled.p sx={{ display: "none" }}>
+			http://essays.davidchouinard.com/the-product-engineer
+			https://blog.pragmaticengineer.com/the-product-minded-engineer/ Over the
+			last few years I've had experience with everything from managing feature
+			requests to managing cloud infrastructure and all the React components,
+			microservices and data stores in between.
+		</Styled.p>
+	</Layout>
+);
 
-class BlogIndex extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMdx.edges
-
-    return (
-      <Fragment>
-        <StickyHeader>
-          <header
-            style={{
-              ...headerBackgroundColor,
-              padding: "6rem 0 4rem",
-            }}
-          >
-            <div style={{ ...centered }}>
-              <h1
-                style={{ marginBottom: "1.5rem" }}
-              >
-                {siteTitle}
-              </h1>
-              <span>
-                Written by <strong>Cadell Christo</strong>.
-              </span>
-            </div>
-          </header>
-        </StickyHeader>
-        <div
-          style={{ position: 'relative', zIndex: 1, backgroundColor: 'white' }}
-        >
-          <div
-            style={{
-              ...centered,
-              paddingTop: "1rem",
-              paddingBottom: "2rem",
-            }}
-          >
-            <SEO title="All posts" />
-            {posts.map(({ node }) => {
-              const title = node.frontmatter.title || node.fields.slug
-              return (
-                <div key={node.fields.slug}>
-                  <h3
-                    style={{
-                      marginBottom: 0,
-                    }}
-                  >
-                    <Link
-                      style={{ textDecoration: 'none' }}
-                      to={node.fields.slug}
-                    >
-                      {title}
-                    </Link>
-                  </h3>
-                  <small style={{display: "block"}}>{node.frontmatter.date}</small>
-                  <p style={{marginTop: "0.25rem"}} dangerouslySetInnerHTML={{ __html: node.frontmatter.description }} />
-                </div>
-              )
-            })}
-          </div>
-        </div>
-      </Fragment>
-    )
-  }
-}
-
-export default BlogIndex
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
-      }
-    }
-  }
-`
+export const query = graphql`
+	query HomeQuery {
+		site {
+			siteMetadata {
+				title
+				social {
+					name
+					url
+				}
+			}
+		}
+	}
+`;
