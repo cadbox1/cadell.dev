01ed4e308a0b579d2f2566c91d22e39a75a6f88e
cadbox1 <hello@cadell.dev>
2022-04-19 20:38:55 +0800

    remove fontBase usage

--- src/components/styles.css.ts
+++ src/components/styles.css.ts
@@ -1,14 +1,11 @@
 import { style } from "@vanilla-extract/css";
-import { fontBase, pClass } from "cadells-vanilla-components";
+import { pClass } from "cadells-vanilla-components";
 
 export const heroClass = style({
 	padding: "64px 0 64px",
 });
 
-export const heroH1Class = style([
-	fontBase,
-	{ fontSize: "42px", marginBottom: "12px" },
-]);
+export const heroH1Class = style({ fontSize: "42px", marginBottom: "12px" });
 
 export const heroTextClass = style([
 	pClass,
