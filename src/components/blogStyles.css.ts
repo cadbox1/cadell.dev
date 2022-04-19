d272dd9399fbe63b6673f269d1f23a25e325384d
cadbox1 <hello@cadell.dev>
2022-04-19 20:45:56 +0800

    use aClass on blog index

--- src/components/blogStyles.css.ts
+++ src/components/blogStyles.css.ts
@@ -1,9 +1,9 @@
 import { style } from "@vanilla-extract/css";
-import { headingClass } from "cadells-vanilla-components";
+import { aClass, headingClass } from "cadells-vanilla-components";
 
 export const blogContainerClass = style({
 	padding: "24px 0 12px",
 	fontFamily: "Source Serif Pro",
 });
 export const blogHeadingClass = style([headingClass, { marginTop: "36px" }]);
-export const blogAnchorClass = style({ textDecoration: "none" });
+export const blogAnchorClass = style([aClass]);
