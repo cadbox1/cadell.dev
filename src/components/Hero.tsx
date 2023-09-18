4cac03bc7931a9088c79578d3f08fd597695faf7
cadbox1 <hello@cadell.dev>
2023-09-18 09:10:12 +0200

    update copy - i like to build things.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,18 +4,17 @@ import { heroClass, heroH1Class, heroTextClass } from "./styles.css";
 export const Hero = () => {
 	return (
 		<div className={heroClass}>
-			<h1 className={heroH1Class}>
-				I build awesome teams and awesome products.
-			</h1>
+			<h1 className={heroH1Class}>I like to build things.</h1>
 			<p className={heroTextClass}>
-				Hello! My name is Cadell Christo and welcome to my website.
+				Hello, my name is Cadell Christo.
 				<br />
 				You can find me on <A href="https://github.com/cadbox1">GitHub</A>,{" "}
 				<A href="https://twitter.com/cadellchristo">Twitter</A> and{" "}
 				<A href="https://www.linkedin.com/in/cadell-christo-551146109/">
 					LinkedIn
 				</A>
-				, or email me at <A href="mailto:hello@cadell.dev">hello@cadell.dev</A>.
+				, and email me at <A href="mailto:hello@cadell.dev">hello@cadell.dev</A>
+				.
 			</p>
 		</div>
 	);
